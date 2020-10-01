c88bfec480452cddfc53d93346260f82edf67dd2
Ryan Anderson <ryanaanderson0@gmail.com>
2020-10-01 15:27:56 -0700

    added main header image and top margins on Image and H1 element

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Ryan from '../assets/Ryan_Resize2.jpg'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
+
 
 
 const HeaderContainer = () => {
@@ -12,8 +14,8 @@ const HeaderContainer = () => {
         <Container fluid>
             <Row>
                 <Col>
-                   <Image src="holder.js/171x180" roundedCircle />
-                   <h1>Hi, I'm Ryan Anderson</h1>
+                   <Image style={{width: "300px", marginTop: "10%"}} src={Ryan} roundedCircle />
+                   <h1 style={{marginTop: "2%"}}>Hi, I'm Ryan Anderson</h1>
                    <h2>Freelance Web Developer</h2>
                 
                 </Col>
