121184745a970de101b30feec26b5748f91a5fa7
Ryan Anderson <ryanaanderson0@gmail.com>
2020-10-06 13:30:37 -0700

    broke the portfolio item into two componenets, PortfolioContainer and PortfolioItem. Also installed and implemented styled-components

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+import PortfolioItem from 'PortfolioItem'
+
+<div style={{backgroundColor: "#343a40" , display: "block"}}>
+<section>
+    <PortfolioItem />
+    <PortfolioItem />
+</section>
+<section>
+    <PortfolioItem />
+    <PortfolioItem />
+    
+</section>
+
+
+    
+</div>
+
+
+export default PortfolioContainer;
--- src/components/PortolioItem.js
+++ src/components/PortolioItem.js
@@ -1,27 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 import UpstageImage from "../assets/upstage.directory_screenshot - Copy.png"
 import BigSmile from "../assets/bigsmile.co_about_crop.png"
 import GoodGood from "../assets/GoodGood_Capture.png"
 import Crumbshot from "../assets/Crumbshot_Capture.png"
-import Image from 'react-bootstrap/Image'
 import './portfolioItem.css'
 
+const Image = styled.img`
+    margin: 1%;
+    width: 35%;
+
+`
 const PortfolioItem = () => {
 
     return( 
-        <div style={{backgroundColor: "#343a40" , display: "block"}}>
-            <section>
-                <Image src={UpstageImage} style={{ margin:"20px", width: "30%"}} rounded />
-                <Image src={BigSmile} style={{ margin:"20px", width: "30%"}} rounded />
-            </section>
-            <section>
-                <Image src={GoodGood} style={{ margin:"20px", width: "30%"}} rounded />
-                <Image src={Crumbshot} style={{ margin:"20px", width: "30%"}} rounded />
-            </section>
-
-            
-                
-        </div>
+       <Image />
     )
 }
 
