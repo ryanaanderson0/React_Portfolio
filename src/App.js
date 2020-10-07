074e0c4c1bea91fb37d3ef01bba07591e2282acc
Ryan Anderson <ryanaanderson0@gmail.com>
2020-10-06 20:06:42 -0700

    resized images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
-import HeaderContainer from './components/Header';
-import NavbarContainer from './components/Navbar';
-import PortfolioItem from './components/PortolioItem';
+import './App.css'
+import HeaderContainer from './components/Header'
+import NavbarContainer from './components/Navbar'
+import PortfolioItem from './components/PortfolioItem'
 
 function App() {
   return (
---
+++ src/components/PortfolioItem.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+import upstageImg from "../assets/upstage.directory_screenshot - Copy.png"
+import bigsmileImg from "../assets/bigsmile.co_about_crop.png"
+import goodgoodImg from "../assets/GoodGood_Capture.png"
+import crumbshotImg from "../assets/Crumbshot_Capture.png"
+
+const Image = styled.img`
+    margin: 2%;
+    border-radius: 15px;
+    width: 40%;
+    height: 40%;
+
+    box-shadow: 10px 30px;
+
+`
+const Div = styled.div`
+    background-color: #343a40;
+
+`
+const PortfolioItem = () => {
+
+    return (
+        <Div>
+            <h1>CURRENT PROJECTS</h1>
+
+            <section>
+                <Image src={upstageImg}/> 
+                <Image src={bigsmileImg} />
+            </section>
+
+            <section>
+                <Image src={goodgoodImg}/> 
+                <Image src={crumbshotImg} />
+            </section>
+           
+         </Div>
+        )
+    
+}
+    
+export default PortfolioItem;
